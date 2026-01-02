4d5744847f97d50a6b50f5de80053e10db9492fa
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-01-02 15:31:18 +0530

    added routers for the city api

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -3,11 +3,15 @@ const bodyparser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
+const ApiRoutes = require('./routes/index');
+
 const setupAndStartServer = async () => {
     const app = express();
 
     app.use(bodyparser.json());
     app.use(bodyparser.urlencoded({extended: true}));
+
+    app.use('/api', ApiRoutes);
 
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`)
