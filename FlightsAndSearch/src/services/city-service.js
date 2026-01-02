1372705eff048f3b8aff35d998c60986572cf4ee
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-01-02 13:56:29 +0530

    added city service

---
+++ FlightsAndSearch/src/services/city-service.js
@@ -0,0 +1,47 @@
+const { CityRepository } = require('../repositroy/index');
+
+class CityService {
+    constructor () {
+        this.cityRepository = new CityRepository();
+    };
+
+    async createCity(data) {
+        try {
+            const city = this.cityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            console.log('Error at Service layer while Creating a city:', error);
+            throw { error };
+        }
+    };
+
+    async deleteCity(cityId) {
+        try {
+            const res = await this.cityRepository.deleteCity(cityId);
+            return res;
+        } catch (error) {
+            console.log('Error at Service layer while Creating a city:', error);
+            throw { error };
+        }
+    };
+
+    async updateCity(cityId, data) {
+        try {
+            const city = this.cityRepository.updateCity(cityId, data);
+            return city;
+        } catch (error) {
+            console.log('Error at Service layer while Creating a city:', error);
+            throw { error };
+        }
+    };
+
+    async getCity(cityId) {
+        try {
+            const city = this.cityRepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log('Error at Service layer while Creating a city:', error);
+            throw { error };
+        }
+    };
+}
