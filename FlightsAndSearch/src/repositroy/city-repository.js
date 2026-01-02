821faeea3ac970c60125eb9aa689d7585d947036
Manish Pardhan <pardhanmanish7887@gmail.com>
2026-01-02 15:37:12 +0530

    minor fixes

--- FlightsAndSearch/src/repositroy/city-repository.js
+++ FlightsAndSearch/src/repositroy/city-repository.js
@@ -27,11 +27,15 @@ class CityRepository {
 
     async updateCity(cityId, data) {
         try {
-            const city = await City.update(data, {
-                where: {
-                    id:cityId
-                }
-            });
+            // if we are using pg then returning true will be usefull else not
+            // const city = await City.update(data, {
+            //     where: {
+            //         id:cityId
+            //     }
+            // });
+            const city =  await City.findByPk(cityId);
+            city?.name = data?.name || '';
+            await city.save();
             return city;
         } catch (error) {
             console.log('Repository layer Error While Update City:', error);
